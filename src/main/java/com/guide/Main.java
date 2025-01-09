319b37a14df6b4606ed29874c0fd559c1e1478c9
alexandre zenas <alexandrezenas11@gmail.com>
2025-01-09 16:29:26 -0300

    :tada: Commit inicial

---
+++ src/main/java/com/guide/Main.java
@@ -0,0 +1,7 @@
+package com.guide;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
---
+++ src/main/java/com/guide/basic_syntax/data_types/DataTypes.java
@@ -0,0 +1,19 @@
+package com.guide.basic_syntax.data_types;
+
+public class DataTypes {
+    public static void main(String[] args) {
+        byte byteMaxValue = 127;
+        short shortMaxValue = 32767;
+        int intMaxValue =  2147483647;
+        long longMaxValue = 1;//9.223.372.036.854.775.807 ;
+        float floatMaxValue = 1 ; //3,4028E + 38
+        double doubleMaxValue = 1 ; //1,7976E + 308
+
+        byte age = 22;
+        short year = 2024;
+        int cep =  25535444;
+        long cpf = 1714548888;
+        float pi = 3.14F; //necessário colocar um F no final
+        double remuneration = 2500.33 ; //1,7976E + 308
+    }
+}
